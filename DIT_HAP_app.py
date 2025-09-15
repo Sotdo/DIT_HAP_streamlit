53ef380d2f8418d084a2eee73146b49053489a8d
Yusheng Yang <yangyusheng12323@outlook.com>
2025-09-15 14:14:27 +0800

    Improve: enhance feature space page in DIT-HAP application by adding an icon for improved navigation

--- DIT_HAP_app.py
+++ DIT_HAP_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 plot_page = st.Page("pages/depletion_data.py", title="Curve plot", icon=":material/timeline:")
-feature_space_page = st.Page("pages/feature_space.py", title="Feature space")
+feature_space_page = st.Page("pages/feature_space.py", title="Feature space", icon=":material/scatter_plot:")
 enrichment_page = st.Page("pages/enrichment_analysis.py", title="Enrichment analysis", icon=":material/search_insights:")
 
 pg = st.navigation(
